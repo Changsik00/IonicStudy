79e54123160ceb9dacdf07baeb6d295d14cef925
Changsik00 <lowmans00@gmail.com>
2017-03-17 15:47:15 +0900

    NavControll Transition Animation 적용

--- MyIonic/src/pages/nav-controll/nav-controll.ts
+++ MyIonic/src/pages/nav-controll/nav-controll.ts
@@ -11,19 +11,25 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'nav-controll.html'
 })
 export class NavControllPage {
-  pushPage;
   params;
   show = false;
+  opts = {animation : "ios-transition"};
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-     this.pushPage = NavControllPage;     
-
      let message = navParams.get('message');
      if(message) {
         let index = navParams.get('index') + 1;
         this.params = { message : message, index : index};
         this.show = true;
      } else {
-       this.params = { message : "this is message", index : 0};
+        this.params = { message : "this is message", index : 0};
      }
+  }
+
+  pushPage() {
+      this.navCtrl.push(NavControllPage, this.params, this.opts);
+  }
+
+  popPage() {
+       this.navCtrl.pop(this.opts);
   }
 }
