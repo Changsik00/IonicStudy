838e0179314d89fefeb91c8b87107f328663f84c
Changsik00 <lowmans00@gmail.com>
2017-03-17 15:47:15 +0900

    NavControll Event를 통해서 데이터 전달

--- MyIonic/src/pages/nav-controll/nav-controll.ts
+++ MyIonic/src/pages/nav-controll/nav-controll.ts
@@ -1,29 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Events } from 'ionic-angular';
 
 /*
   https://ionicframework.com/docs/v2/api/navigation/NavController/
   https://ionicframework.com/docs/v2/api/components/nav/NavPush/
   https://ionicframework.com/docs/v2/api/components/nav/NavPop/
+
+  https://ionicframework.com/docs/v2/api/util/Events/
 */
 @Component({
-  selector: 'page-nav-controll',
-  templateUrl: 'nav-controll.html'
+    selector: 'page-nav-controll',
+    templateUrl: 'nav-controll.html'
 })
 export class NavControllPage {
-  pushPage;
-  params;
-  show = false;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-     this.pushPage = NavControllPage;     
+    params;
+    show = false;
+    opts = { animation: "ios-transition" };
 
-     let message = navParams.get('message');
-     if(message) {
-        let index = navParams.get('index') + 1;
-        this.params = { message : message, index : index};
-        this.show = true;
-     } else {
-       this.params = { message : "this is message", index : 0};
-     }
-  }
+    index;
+    constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events) {
+        let message = navParams.get('message');
+        if (message) {
+            this.index = navParams.get('index') + 1;
+            this.params = { message: message, index: this.index };
+            this.show = true;
+
+            events.subscribe('user:created', (user, time) => {
+                console.log(`Welcome, ${user} , at  ${time}`);
+            });
+
+        } else {
+            this.events.unsubscribe('user:created');
+            this.params = { message: "this is message", index: 0 };
+        }
+
+
+    }
+
+    pushPage() {
+        this.navCtrl.push(NavControllPage, this.params, this.opts);
+    }
+
+    popPage() {
+        this.events.publish('user:created', `user ${this.index}`, Date.now());
+        this.navCtrl.pop(this.opts);
+    }
 }
