c23ac190b4d247c8b73d5569c1b6bb072f84da3c
Changsik00 <lowmans00@gmail.com>
2017-03-29 13:14:52 +0900

    Passing  Data 페이지 이동간 데이터 통신 테스트

--- MyIonic_v1/www/js/controllers.js
+++ MyIonic_v1/www/js/controllers.js
@@ -1,16 +1,20 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $location) {
+.controller('AppCtrl', function($scope, $state) {
   $scope.$on('$ionicView.enter', function(e) {
       console.log("#@# AppCtrl $ionicView.enter");
   });
 
   $scope.goNext = function(index) {
-      $location.path('/home')
+      $state.go('home',{data:"testData"});
   }
 })
 
-.controller('HomeCtrl', function($scope, $state) {
+.controller('HomeCtrl', function($scope, $state, $stateParams) {
+  $scope.$on('$ionicView.enter', function(e) {
+      console.log("#@# $stateParams " + $stateParams.data);
+  });
+
   $scope.goPrev = function() {
       $state.go('app'); // '/app' 으로 하면 Error: Could not resolve '/app' from state 'home'
   }
